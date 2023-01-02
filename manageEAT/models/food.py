206f9797e083907288482862d3558db84ec71367
kaaviya98 <kaaviya@testpress.in>
2023-01-02 17:09:00 +0530

    Refactor models

--- manageEAT/models/food.py
+++ manageEAT/models/food.py
@@ -18,7 +18,7 @@ MENU_TYPES = [
 class Food(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    image = models.ImageField(upload_to="media/%Y/%m/%d/")
     description = models.TextField(max_length=50, blank=True)
     food_type = models.CharField(max_length=10, choices=FOOD_TYPES, default="Veg")
     menu_type = models.CharField(max_length=10, choices=MENU_TYPES, default="Breakfast")
